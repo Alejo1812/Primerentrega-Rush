8ab6741e490b166359d104a338bf8e41966b081d
Alejo1812 <alejorush66@gmail.com>
2023-09-28 21:06:59 -0300

    entrega 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,81 @@
-function billetera(saldo){
-    if (saldo > 0) 
-        alert ("tiene $" + saldo)
-    else if (saldo==0)
-        alert ("no tiene un mango")
-    else if (saldo < 0)
-        alert ("esta en negativo")
-}
-saldo=prompt("ingrese su saldo")
-console.log(billetera(saldo))
-var i=0
-var comprado=0
-function compra(saldo,cantidad){
-    for (i=1;i<=cantidad;i++){
-        if(saldo>=10){
-            saldo=saldo-10;
-            comprado=comprado+1}
-        else{
-            alert("no tiene mas dinero");
-            break}
-        
+// Objeto que representa la cuenta personal
+const cuentaPersonal = {
+    montoInicial: 0,
+    gastos: [],
+    agregarGasto: function (nombre, cantidad) {
+      // Buscar si ya existe un gasto con el mismo nombre
+      const gastoExistente = this.gastos.find(gasto => gasto.nombre === nombre);
+  
+      if (gastoExistente) {
+        // Si existe, actualizar la cantidad
+        gastoExistente.cantidad += cantidad;
+      } else {
+        // Si no existe, agregar un nuevo gasto
+        this.gastos.push({ nombre, cantidad });
+      }
+    },
+    calcularSaldo: function () {
+      const totalGastos = this.gastos.reduce((total, gasto) => total + gasto.cantidad, 0);
+      return this.montoInicial - totalGastos;
+    },
+  };
+  
+  // Función para establecer el monto inicial
+  function establecerMontoInicial() {
+    cuentaPersonal.montoInicial = parseFloat(prompt("Ingrese el monto inicial de la cuenta:"));
+  
+    if (isNaN(cuentaPersonal.montoInicial)) {
+      console.log("Por favor, ingrese un monto inicial válido.");
+      establecerMontoInicial();
     }
-    alert("usted compro " + comprado)
-    alert("le quedan $" +saldo)
-}
-cantidad=prompt("cada item vale 10, cuanto quiere comprar?")
-console.log(compra(saldo,cantidad))
+  }
+  
+  // Función para agregar gastos hasta que no haya más dinero
+  function simularCuenta() {
+    while (true) {
+      let nombreGasto = prompt("Ingrese el nombre del gasto:");
+      let cantidadGasto = parseFloat(prompt("Ingrese la cantidad del gasto:"));
+  
+      if (isNaN(cantidadGasto)) {
+        console.log("Por favor, ingrese una cantidad válida.");
+        continue;
+      }
+  
+      cuentaPersonal.agregarGasto(nombreGasto, cantidadGasto);
+      console.log(`Gasto de ${cantidadGasto} registrado para ${nombreGasto}.`);
+  
+      const respuesta = prompt("¿Quiere agregar otro gasto? (Ingrese 'si' para sí, cualquier otra tecla para no)");
+  
+      if (respuesta.toLowerCase() !== 'si') {
+        break;
+      }
+    }
+  
+    mostrarResumen();
+  }
+  
+  // Función para mostrar un resumen de la cuenta
+  function mostrarResumen() {
+    console.log("Resumen de la cuenta:");
+    console.log(`Monto inicial: ${cuentaPersonal.montoInicial}`);
+    console.log("Gastos:");
+  
+    if (cuentaPersonal.gastos.length === 0) {
+      console.log("No se registraron gastos.");
+    } else {
+      cuentaPersonal.gastos.forEach((gasto) => {
+        console.log(`${gasto.nombre}: ${gasto.cantidad}`);
+      });
+    }
+  
+    console.log(`Saldo restante: ${cuentaPersonal.calcularSaldo()}`);
+  
+    if (cuentaPersonal.calcularSaldo() <= 0) {
+      console.log("¡Se ha agotado el dinero! No puedes agregar más gastos.");
+    }
+  }
+  
+  // Iniciar la simulación
+  establecerMontoInicial();
+  simularCuenta();
+  
