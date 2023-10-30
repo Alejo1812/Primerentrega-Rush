5b8c049600a5ba32d782faf934fc3ae8d31d75e5
Alejo1812 <alejorush66@gmail.com>
2023-10-29 21:53:03 -0300

    entrega final en proceso

--- scripts/main.js
+++ scripts/main.js
@@ -1,107 +1,92 @@
 let cuentaPersonal = {
-    montoInicial: 0,
-    gastos: [],
-    agregarGasto: function (nombre, cantidad) {
-      const gastoExistente = this.gastos.find(gasto => gasto.nombre === nombre);
-  
-      if (gastoExistente) {
-        gastoExistente.cantidad += cantidad;
-        gastoExistente.fecha = obtenerFechaActual();
-      } else {
-        this.gastos.push({ nombre, cantidad, fecha: obtenerFechaActual() });
-      }
-    },
-    eliminarGasto: function (nombre) {
-      this.gastos = this.gastos.filter(gasto => gasto.nombre !== nombre);
-    },
-    calcularSaldo: function () {
-      const totalGastos = this.gastos.reduce((total, gasto) => total + gasto.cantidad, 0);
-      return this.montoInicial - totalGastos;
-    },
-    reiniciar: function () {
-      this.montoInicial = 0;
-      this.gastos = [];
-    },
-  };
-  
-  function obtenerFechaActual() {
-    const fecha = new Date();
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
-    return fecha.toLocaleDateString('es-ES', options);
+  montoInicial: 0,
+  gastos: [],
+  agregarGasto: function (nombre, cantidad) {
+    this.gastos.push({ nombre, cantidad });
+  },
+  calcularSaldo: function () {
+    const totalGastos = this.gastos.reduce((total, gasto) => total + gasto.cantidad, 0);
+    return this.montoInicial - totalGastos;
+  },
+};
+
+function iniciarSimulador() {
+  cuentaPersonal.montoInicial = parseFloat(document.getElementById("montoInicial").value);
+
+  if (isNaN(cuentaPersonal.montoInicial)) {
+    alert("Por favor, ingrese un monto inicial válido.");
   }
-  
-  function iniciarSimulador() {
-    cuentaPersonal.montoInicial = parseFloat(document.getElementById("montoInicial").value);
-  
-    if (isNaN(cuentaPersonal.montoInicial)) {
-      alert("Por favor, ingrese un monto inicial válido.");
-    } else {
-      localStorage.setItem("cuentaPersonal", JSON.stringify(cuentaPersonal));
-      mostrarResumen();
-    }
+}
+
+function agregarGasto() {
+  const nombreGasto = document.getElementById("nombreGasto").value;
+  const cantidadGasto = parseFloat(document.getElementById("cantidadGasto").value);
+
+  if (!validarNombre(nombreGasto)) {
+    alert("Nombre de gasto inválido. Por favor, evita caracteres especiales.");
+    return;
   }
-  
-  function agregarGasto() {
-    const nombreGasto = document.getElementById("nombreGasto").value;
-    const cantidadGasto = parseFloat(document.getElementById("cantidadGasto").value);
-  
-    if (isNaN(cantidadGasto)) {
-      alert("Por favor, ingrese una cantidad válida.");
-    } else {
-      cuentaPersonal.agregarGasto(nombreGasto, cantidadGasto);
-      localStorage.setItem("cuentaPersonal", JSON.stringify(cuentaPersonal));
-      mostrarResumen();
-    }
+
+  if (!validarCantidad(cantidadGasto)) {
+    alert("Cantidad de gasto inválida. Por favor, ingresa un número válido.");
+    return;
   }
-  
-  function eliminarGasto(nombreGasto) {
-    cuentaPersonal.eliminarGasto(nombreGasto);
-    localStorage.setItem("cuentaPersonal", JSON.stringify(cuentaPersonal));
-    mostrarResumen();
+
+  cuentaPersonal.agregarGasto(nombreGasto, cantidadGasto);
+  mostrarResumen();
+}
+
+function mostrarResumen() {
+  document.getElementById("montoInicialResumen").textContent = `Monto Inicial: $${cuentaPersonal.montoInicial}`;
+  document.getElementById("saldoResumen").textContent = `Saldo Restante: $${cuentaPersonal.calcularSaldo()}`;
+
+  if (cuentaPersonal.calcularSaldo() <= 0) {
+    document.getElementById("mensajeFinal").textContent = "¡Saldo Agotado!";
+  } else {
+    document.getElementById("mensajeFinal").textContent = "";
   }
-  
-  function reiniciarSimulador() {
-    cuentaPersonal.reiniciar();
-    localStorage.removeItem("cuentaPersonal");
-    limpiarFormulario();
-    mostrarResumen();
-  }
-  
-  function limpiarFormulario() {
-    document.getElementById("montoInicial").value = "";
-    document.getElementById("nombreGasto").value = "";
-    document.getElementById("cantidadGasto").value = "";
-  }
-  
-  function mostrarResumen() {
-    const montoInicialResumen = document.getElementById("montoInicialResumen");
-    const gastosResumen = document.getElementById("gastosResumen");
-    const saldoResumen = document.getElementById("saldoResumen");
-    const mensajeFinal = document.getElementById("mensajeFinal");
-  
-    montoInicialResumen.textContent = `Monto inicial: ${cuentaPersonal.montoInicial}`;
-    gastosResumen.innerHTML = "Gastos:<br>" +
-      (cuentaPersonal.gastos.length === 0 ? "No se registraron gastos." :
-        cuentaPersonal.gastos.map(gasto =>
-          `${gasto.nombre}: ${gasto.cantidad} (${gasto.fecha})
-          <button onclick="eliminarGasto('${gasto.nombre}')">Eliminar</button>`
-        ).join('<br>'));
-  
-    saldoResumen.textContent = `Saldo restante: ${cuentaPersonal.calcularSaldo()}`;
-  
-    if (cuentaPersonal.calcularSaldo() <= 0) {
-      mensajeFinal.textContent = "¡Se ha agotado el dinero! No puedes agregar más gastos.";
-    } else {
-      mensajeFinal.textContent = "";
-    }
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const storedCuenta = localStorage.getItem("cuentaPersonal");
-  
-    if (storedCuenta) {
-      cuentaPersonal = JSON.parse(storedCuenta);
-      mostrarResumen();
-    }
+
+  mostrarGastosOrdenados();
+}
+
+function mostrarGastosOrdenados() {
+  const gastosOrdenados = cuentaPersonal.gastos.slice().sort((a, b) => b.cantidad - a.cantidad);
+  const gastosDiv = document.getElementById("gastosResumen");
+  gastosDiv.innerHTML = "";
+
+  gastosOrdenados.forEach(gasto => {
+    const gastoElement = document.createElement("p");
+    gastoElement.textContent = `${gasto.nombre}: $${gasto.cantidad}`;
+    gastosDiv.appendChild(gastoElement);
   });
-  
+}
+
+function guardarEnLocalStorage() {
+  localStorage.setItem("cuentaPersonal", JSON.stringify(cuentaPersonal));
+}
+
+function cargarDesdeLocalStorage() {
+  const cuentaGuardada = localStorage.getItem("cuentaPersonal");
+  if (cuentaGuardada) {
+    const cuentaGuardadaJSON = JSON.parse(cuentaGuardada);
+    cuentaPersonal.montoInicial = cuentaGuardadaJSON.montoInicial;
+    cuentaPersonal.gastos = cuentaGuardadaJSON.gastos;
+    mostrarResumen();
+  }
+}
+
+function validarNombre(nombre) {
+  const regex = /^[a-zA-Z0-9\s.,-]+$/;
+  return regex.test(nombre);
+}
+
+function validarCantidad(cantidad) {
+  const regex = /^\d*\.?\d*$/;
+  return regex.test(cantidad);
+}
+
+document.getElementById("iniciarSimulador").addEventListener("click", iniciarSimulador);
+document.getElementById("agregarGasto").addEventListener("click", agregarGasto);
+
+// Cargar los datos al iniciar la aplicación
+cargarDesdeLocalStorage();
